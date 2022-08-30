49bf725e5226e02119ea9b2c58946e566febc160
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2022-08-30 12:35:09 +0500

    stylint fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,23 @@
 const { src, dest, parallel } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
-const package = require("./package.json");
+const package = require('./package.json');
 const rename = require('gulp-rename');
 
 // Get Plugin Version from `package.json`
-const pluginVersion = package.preventXssVulnerability.pluginVersion;
+const pluginVersion = package.httpAuth.pluginVersion;
 
 async function deleteMinFiles() {
   await del(['admin/**/*.min.css', 'frontend/**/*.min.css']);
 }
 
 function minifyCss() {
-  return src(
-      [
-        'admin/**/*.css',
-        'frontend/**/*.css',
-        '!admin/**/*/*.min.css',
-        '!frontend/**/*.min.css'
-      ],
-      {
-        base: './'
-      }
-    )
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename(function(path) {
-        path.extname = '-' + pluginVersion + '.min' + path.extname;
+  return src(['assets/**/*.css', '!assets/**/*/*.min.css'])
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename(function (path) {
+      path.extname = '-' + pluginVersion + '.min' + path.extname;
     }))
-    .pipe(dest('./'));
+    .pipe(dest('assets/'));
 }
 
 exports.build = parallel(deleteMinFiles, minifyCss);
